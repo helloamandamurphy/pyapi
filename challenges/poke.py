1b37e2dddf617fc37f76c8629166b38c0f1160b7
Amanda Murphy <helloamandamurphy@gmail.com>
2020-10-01 15:48:24 +0000

    10/1 Morning Challenge PokemonAPI add extra method

--- challenges/poke.py
+++ challenges/poke.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
   
 import requests
+import wget
 
 base_url =  "https://pokeapi.co/api/v2/pokemon/"
 
+#ASSIGNED
 def api_pull():
     choice= input("What Pokemon would you like a picture of? ")
     poke_url= base_url + choice.lower()
     pokedikt= requests.get(poke_url).json()
     return pokedikt
 
+#ASSIGNED
 def api_slice(pokeinfo):
     poke_pic= pokeinfo['sprites']['front_default']
     return poke_pic
     #print(poke_pic)
 
-
+#EXTRA
+def wget_pic(imagelink):
+    wget.download(imagelink, '/home/student/static/')
 
 def main():
-    pokeinfo= api_pull()
-    api_slice(pokeinfo)
+    wget_pic(api_slice(api_pull))
 
 if __name__ == "__main__":
     main()
