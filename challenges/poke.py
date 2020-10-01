1f7d8ad1e38bbd9b36a8ddf217954e03254c76c7
Amanda Murphy <helloamandamurphy@gmail.com>
2020-10-01 15:29:15 +0000

    10/1 Morning Challenge PokemonAPI

---
+++ challenges/poke.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+  
+import requests
+
+base_url =  "https://pokeapi.co/api/v2/pokemon/"
+
+def api_pull():
+    choice= input("What Pokemon would you like a picture of? ")
+    poke_url= base_url + choice.lower()
+    pokedikt= requests.get(poke_url).json()
+    return pokedikt
+
+def api_slice(pokeinfo):
+    poke_pic= pokeinfo['sprites']['front_default']
+   # return poke_pic
+    print(poke_pic)
+
+def main():
+    pokeinfo= api_pull()
+    api_slice(pokeinfo)
+
+if __name__ == "__main__":
+    main()
